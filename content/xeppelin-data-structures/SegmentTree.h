34a7fd9494cd7c5c985a1add4a2ba902f02019e0
Konstantin Amelichev <kostya.amelichev@gmail.com>
2024-11-25 23:23:00 +0100

    move stuff from one place to another

--- content/xeppelin-data-structures/SegmentTree.h
+++ content/xeppelin-data-structures/SegmentTree.h
@@ -23,19 +23,31 @@ int get(int l, int r) {
 	res = min(res, t[l]);}
 
 // if persistent: store left and right child and return updated node from upd to attach in place of previous version
-void upd(int v, int tl, int tr, int pos, int x) { // dont use with pushes!!
+// dont use single position update with pushes!!
+void upd(int v, int tl, int tr, int pos, int x) { 
 	if (tl == tr) {t[v] = x; return;}
 	int tm = (tl + tr) / 2;
 	if (pos <= tm) upd(2 * v, tl, tm, pos, x); else upd(2 * v + 1, tm + 1, tr, pos, x);
 	t[v] = min(t[2 * v], t[2 * v + 1]);
 }
 
-// same code for get, just return value instead of updating
-void upd(int v, int tl, int tr, int l, int r, int x) {
+// gets position with sum <= cnt
+pair<int, int> get(int v, int tl, int tr, int l, int r, int cnt, int tp) {
 	push(v, tl, tr);
-	if (l > tr || tl > r) return;
-	if (l <= tl && tr <= r) {t[v] = x; push(v, tl, tr); return;}
+	if (r < tl || l > tr) {
+		return {-1, 0};
+	}
+	if (l <= tl && r >= tr && cnt > t[v]) {
+		return {-1, t[v]};
+	}
+	if (tl == tr) {
+		return {tl, 0};
+	}
+	// lazy: if (!v->l) create_lr(v); 
 	int tm = (tl + tr) / 2;
-	if (pos <= tm) upd(2 * v, tl, tm, l, r, x); else upd(2 * v + 1, tm + 1, tr, l, r, x);
-	t[v] = min(t[2 * v], t[2 * v + 1]);
+	auto p1 = get(v * 2, tl, tm, l, r, cnt, tp);
+	if (p1.first != -1) return p1;
+	auto p3 = get(v * 2 + 1, tm + 1, tr, l, r, cnt - p1.second, tp);
+	p3.second += p1.second;
+	return p3;
 }
