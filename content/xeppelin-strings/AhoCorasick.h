fb7a52f947d1db65d1aad336428e2efb2aecb657
Konstantin Amelichev <kostya.amelichev@gmail.com>
2025-08-27 11:05:50 +0200

    Update AhoCorasick.h

--- content/xeppelin-strings/AhoCorasick.h
+++ content/xeppelin-strings/AhoCorasick.h
@@ -6,18 +6,28 @@
  */
 #pragma once
 
-
-struct V{ int parent; int pchar; int link; int go[26]; /*for fast answer int term_link;*/ };
-
-queue<int> q; q.push(0);
-for (q.push(0); !q.empty(); q.pop()) {
-	V v = /*cast*/ q.front();
-	for (/* char c */) {
-		// can also do while(go[p][c] == -1) p = suflink[p]
-		// but doesnt build explisit automaton 
-		suflink[v] = go[suflink[parent[v]]][pchar[v]];
-		tree[suflink[v]].push_back(v);
-		if (go[v][c] == -1) go[v][c] = go[suflink[v]][c];
-		else q.push(go[v][c]);
-	}
+void build_automaton() {
+    queue<int> q;
+    G[0].suflink = 0;
+    for (int c = 0; c < 26; ++c) {
+        int u = G[0].go[c];
+        if (u == -1) G[0].go[c] = 0;
+    	else {
+            G[u].suflink = 0; G[0].tree.push_back(u); // suffix tree edge
+			q.push(u);
+        }
+    }
+    while (!q.empty()) {
+        int v = q.front(); q.pop();
+        for (int c = 0; c < 26; ++c) {
+            int u = G[v].go[c];
+            if (u == -1)
+                G[v].go[c] = G[G[v].suflink].go[c];
+            else {
+                G[u].suflink = G[G[v].suflink].go[c];
+                G[G[u].suflink].tree.push_back(u); // suffix tree edge
+                q.push(u);
+            }
+        }
+    }
 }
